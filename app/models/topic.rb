258b97bcb336b9108c27e0f0368b9826cb239f80
devzolotorevich@gmail.com <devzolotorevich@gmail.com>
2022-09-21 11:25:05 +0300

    fix another little mistake

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,4 @@
 class Topic < ApplicationRecord  #ApplicationRecord < ActiveRecord
-    validates :title, presence :true, length: {minimum:2, maximum:256} #Валидация заголовка на его наличие и длину более 2
-    validates :body, presence :true, length: {minimum:2, maximum:65},
+    validates :title, presence: true, length: {minimum:2} #Валидация заголовка на его наличие и длину более 2
+    validates :body, presence: true, length: {minimum:2}
 end
