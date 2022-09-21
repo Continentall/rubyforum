cfb94f18ee6d8d59c4c19f878e927237679d382b
Ivan <Continentall@users.noreply.github.com>
2022-09-21 11:06:22 +0300

    i fix little mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    get '/topics', to: 'topics#index'
+
     root 'pages#index' #root - корневой маршрут / (example.com/), pages -имя контроллера который надо вызвать , index - метод котроллера к которому обращаемся
-    get '/topics', to 'topics#index'
 end
