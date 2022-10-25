c1b880c7ec16a6d5efdf2c098b123d10b8af9d35
Ivan <Continentall@users.noreply.github.com>
2022-10-25 12:28:10 +0300

    tags looks preety good

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,10 @@
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 
-import $ from 'jquery'
-import 'select2/dist/js/select2'
-
 import '@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
+import './scripts/select2'
 
 Rails.start()
 Turbolinks.start()
